7b743cf8cbbe17cbd74f6854fd1ab1472881a84d
Gosia2024 <lm.sprachen@gmail.com>
2025-10-29 12:02:18 +0100

    feat: render products in the table and calcultate total price

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,20 @@
-const product1 = { price: 10 };
-const product2 = { price: 'fasdf' };
+const product1 = { price: 10, title: 'JS od podstaw' };
+const product2 = { price: '20', title: 'PHP od podstaw' };
 const discount = 10;
 
+//add products to table
+const itemsContainer = document.querySelector('#items')
+itemsContainer.innerHTML += `   <tr>
+        <td>${product1.title}</td>
+        <td>1</td>
+        <td>${product1.price}</td>
+      </tr>`
+itemsContainer.innerHTML += ` <tr>
+        <td>${product2.title}</td>
+        <td>1</td>
+        <td>${product2.price}</td>
+      </tr>
+`
 
 
 if (isNaN (+ product2.price)){
@@ -11,7 +24,7 @@ if (isNaN (+ product2.price)){
 const total = + product1.price + + product2.price;
 const totalWithDiscount = total - 10;
 
-
+document.querySelector('#total-price').innerHTML = total
 
 console.log(`Cena przed znizka:  ${total} 
     Cena po znizce: ${total}`);
